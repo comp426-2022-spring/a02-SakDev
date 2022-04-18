6810836b81070d4e40e19df58bd0a7f834e45da5
SakDev <SakDev@unc.edu>
2022-04-18 16:46:47 -0400

    idek anymore

--- flips.js
+++ flips.js
@@ -1,5 +1,4 @@
 import { coinFlips } from "./modules/coin.mjs";
-import { coinFlip } from "./modules/coin.mjs";
 import { countFlips } from "./modules/coin.mjs";
 
 import minimist from 'minimist'
@@ -7,7 +6,7 @@ import minimist from 'minimist'
 const args = minimist(process.argv.slice(2))
 const num  = args['number'] //|| 1
 
-var flips = coinFlips(num)
+let flips = coinFlips(num);
 console.log(flips)
 console.log(countFlips(flips))
-console.log(args)
+// console.log(args)
---
+++ guess-flip.js
@@ -0,0 +1,11 @@
+import { flipACoin } from "./modules/coin.mjs";
+import minimist from "minimist";
+
+const args = minimist(process.argv.slice(2));
+const call = args['call'];
+
+if (call == "heads" || call == "tails") {
+    console.log(flipACoin(call));
+} else {
+    console.log("Error: no inputs\nUsage: node guess-flip --call=[heads|tails]");
+}
